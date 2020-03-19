4d46c4d3708d53cd49947823fe8d90c662c69674
Elijah Wigmore <elwigmor@microsoft.com>
2020-03-19 15:35:22 -0700

    Added project CompareStandings.Core, made initial versions of classes Game and Team.

---
+++ CompareStandings.Core/Game.cs
@@ -0,0 +1,15 @@
+﻿namespace CompareStandings.Core
+{
+    public class Game
+    {
+        public int ID { get; set; }
+
+        public int HomeTeamID { get; set; }
+
+        public int AwayTeamID { get; set; }
+
+        public int HomeTeamPoints { get; set; }
+
+        public int AwayTeamPoints { get; set; }
+    }
+}
